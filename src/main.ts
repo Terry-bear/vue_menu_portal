4dee6a163524e149aa3b0173eb406dcba7eee58e
Terry-zh <496971418@qq.com>
2019-02-13 17:06:10 +0800

    add vuex demo

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element'
-import { makeReduxStore } from './plugins/redux-entry'
-import reduxPlugin from './plugins/redux-entry'
-import reduce from './store'
-import logger from 'redux-logger'
-import { Store, AnyAction } from 'redux'
 
 Vue.config.productionTip = false
-Vue.use(reduxPlugin)
-const store: Store<{}, AnyAction> = makeReduxStore(reduce, [logger])
 new Vue({
   // store,
   render: (h) => h(App)
